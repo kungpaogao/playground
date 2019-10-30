2137d725afd0ac3507c90702db7e2b8c1623758b
Andrew <kungpaogao@gmail.com>
2019-10-29 21:10:01 -0400

    Implemented better update, reset; some docs

--- src/tictactoe/ttt.js
+++ src/tictactoe/ttt.js
@@ -1,17 +1,52 @@
 console.log("loaded");
 
-let squares = document.getElementsByClassName("square");
+let squares = Array.from(document.getElementsByClassName("square"));
 let state = [];
+let isX = true;
 
-function update(id) {
-  // let button = document.getElementById(id);
-  state.push(id);
+/**
+ * Checks if `state` contains a value for square `id`
+ * @param {string} id
+ */
+function hasKey(id) {
+  return state.map(sq => sq.k).indexOf(id) !== -1;
 }
 
-function assert(pass, testName) {
+function updateText(id, txt) {
+  let button = document.getElementById(id);
+  button.childNodes.forEach(node => (node.innerText = txt));
+}
+
+/**
+ * Updates `state` when a square is triggered
+ * @param {string} id
+ */
+function update(id) {
+  let l = isX ? "X" : "O";
+  if (!hasKey(id)) {
+    updateText(id, l);
+    state.push({ k: id, v: l });
+    isX = !isX;
+  }
+
+  console.log("state", state);
+}
+
+function reset() {
+  squares.forEach(sq => updateText(sq.id, ""));
+  isX = true;
+  state = [];
+}
+
+/**
+ * Creates a test case display named `name`; green if `pass`, red otherwise
+ * @param {boolean} pass
+ * @param {string} name
+ */
+function assert(pass, name) {
   let elem = document.createElement("div");
   elem.className = "row";
-  elem.innerText = testName;
+  elem.innerText = name;
 
   if (pass) {
     elem.style.background = "lightgreen";
@@ -22,6 +57,10 @@ function assert(pass, testName) {
   document.body.appendChild(elem);
 }
 
+/**
+ * Creates header titled `title` for a set of test cases
+ * @param {string} title
+ */
 function testHead(title) {
   let elem = document.createElement("div");
   elem.className = "row";
@@ -36,7 +75,16 @@ assert(true, "test pass");
 assert(false, "test fail");
 
 testHead("state cases");
-update("1");
-update("2");
-update("3");
-assert(state.length === 3, "testing update adds to state");
+update("sq1");
+update("sq2");
+update("sq3");
+assert(state.length === 3, "update adds to state");
+
+reset();
+update("sq1");
+update("sq1");
+update("sq1");
+update("sq1");
+assert(state.length === 1, "no duplicates in state");
+
+reset();
